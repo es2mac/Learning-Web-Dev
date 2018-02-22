c4dd8634fac61b9b107aff8868cb955105891025
Hsing Liu <liu.hsing@gmail.com>
2018-02-22 13:18:42 +0800

    Finish section 9

---
+++ Section 9 JS Loops/for_script.js
@@ -0,0 +1,30 @@
+
+console.log("Problem 1");
+for (let i = -10; i < 20; i++) {
+    console.log(i)
+}
+
+
+console.log("");
+console.log("Problem 2");
+for (let i = 10; i <= 40; i += 2) {
+    console.log(i)
+}
+
+console.log("");
+console.log("Problem 3");
+for (let i = 300; i <= 333; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+}
+
+console.log("");
+console.log("Problem 4");
+for (let i = 5; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i)
+    }
+}
+
+
