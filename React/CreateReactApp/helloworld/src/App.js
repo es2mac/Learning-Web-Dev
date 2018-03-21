a3129009c9f4c5e4b72a00b9a095420f50d2ecf6
Hsing Liu <liu.hsing@gmail.com>
2018-03-21 17:39:03 +0800

    More React suff

---
+++ React/CreateReactApp/helloworld/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Hello RWorld</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    Another paragraph: {this.props.mystery}
+                </p>
+            </div>
+        );
+    }
+}
+// {this.props.mystery}
+
+export default App;
